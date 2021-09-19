0a3f6110cb40b45f85265d704b71665f5d356259
www-data <www-data@vm-7ef61cc9.netangels.ru>
2021-09-19 02:38:19 +0000

    New mail message format

--- disk_monitor.js
+++ disk_monitor.js
@@ -16,10 +16,11 @@ function on_check(info) {
     info.usedPercent = 100 / info.total * info.used;
     info.availablePercent = 100 / info.total * info.available;
 
-    const isWarning = info.usedPercent > (Number(config.warningLevel) || 50);
+    const isWarning = info.usedPercent > (Number(config.warningLevel) || 70);
     const isDanger = info.usedPercent > (Number(config.dangerLevel) || 80);
-    const isFatal = info.usedPercent > (Number(config.fatalLevel) || 95);
-    const barColor = isFatal ? '#8b0000' : isDanger ? '#dc143c' : isWarning ? '#ffd700' : '#3cb371';
+    const isFatal = info.usedPercent > (Number(config.fatalLevel) || 90);
+    const barBgColor = isFatal ? '#8b0000' : isDanger ? '#dc143c' : isWarning ? '#ffd700' : '#3cb371';
+    const barColor = isDanger ? '#fff' : '#000';
     const status = isFatal ? 'Fatal' : isDanger ? 'Danger' : isWarning ? 'Warning' : 'Normal';
 
     console.log(chalk.yellow('Used: '), (info.used / GB).toFixed(3), 'Gb,', info.usedPercent.toFixed(1) + '%');
@@ -32,45 +33,49 @@ function on_check(info) {
     // send email only when warning status
     if (!isWarning) return;
 
+    const emoji = isFatal ? '🔴' : isDanger ? '🔶' : '👋';
+
     const transport = nodeMailer.createTransport(config.mailTransport);
     const mailOptions = {
 	from: config.from,
 	to: config.to,
-	subject: `${status} - ${config.name} disk usage`,
+	subject: `${emoji} [${status}] ${config.name} disk usage`,
 	text: `${status} disk space usage on server ${config.name}.
 Technical info:
 ${JSON.stringify(info, true, '\t')}
 `,
-	html: `<div style="font: 15pt Arial">
-<h1>${status} <u>${config.name}</u> disk space usage</h1>
-<p>
-    Used <big><strong>${info.usedPercent.toFixed(1)}%</big></strong>,
-    <big><strong>${(info.used / GB).toFixed(3)} Gb</big></strong> from <big>${(info.total / GB).toFixed(3)} Gb</big>
-</p>
-<div style="background: silver; width: 100%; padding: 1px;">
-    <div style="background: ${barColor}; width: ${(info.usedPercent).toFixed(1)}%; min-height: 20px; text-align: center; padding: 10px 0;">
-	${(info.usedPercent).toFixed(3)}%
+	html: `<div style="font: 13pt Arial">
+<div style="font-size: 16pt; font-weight: bold; margin-bottom: 15px;">${status} <u>${config.name}</u> disk space usage</div>
+<div style="margin-bottom: 20px;">
+    <p>
+	Used <big><strong>${(info.used / GB).toFixed(1)} Gb</big></strong> from <big>${(info.total / GB).toFixed(1)} Gb</big>
+    </p>
+    <div style="background: silver; width: 100%; max-width: 400px; border: 1px solid black; border-radius: 4px;">
+	<div style="background: ${barBgColor}; color: ${barColor}; width: ${(info.usedPercent).toFixed(1)}%; line-height: 15px; font-size: 10pt; text-align: center; padding: 2px 0; border-radius: 3px;">
+	    ${(info.usedPercent).toFixed(3)}%
+	</div>
     </div>
 </div>
-<p>
-    Available <big><strong>${info.availablePercent.toFixed(1)}%</big></strong>,
-    <big><strong>${(info.available / GB).toFixed(3)} Gb</big></strong> from <big>${(info.total / GB).toFixed(3)} Gb</big>
-</p>
-<div style="background: silver; width: 100%; padding: 1px;">
-    <div style="background: ${barColor}; width: ${(info.availablePercent).toFixed(1)}%; min-height: 20px; text-align: center; padding: 10px 0;">
-	${(info.availablePercent).toFixed(3)}%
+<div style="margin-bottom: 20px;">
+    <p>
+	Available <big><strong>${(info.available / GB).toFixed(1)} Gb</big></strong> from <big>${(info.total / GB).toFixed(1)} Gb</big>
+    </p>
+    <div style="background: silver; width: 100%; max-width: 400px; border: 1px solid black; border-radius: 4px;">
+	<div style="background: ${barBgColor}; color: ${barColor}; width: ${(info.availablePercent).toFixed(1)}%; line-height: 15px; font-size: 10pt; text-align: center; padding: 2px 0; border-radius: 3px;">
+	    ${(info.availablePercent).toFixed(3)}%
+        </div>
     </div>
 </div>
-<br/>
-<h3>Technical information:</h3>
-<pre>${JSON.stringify(info, true, '\t')}</pre>
+<div style="font-size: 14pt;">Technical information:</div>
+<pre style="font: 12pt Courier New">${JSON.stringify(info, true, '  ')}</pre>
 `,
     };
+
     transport.sendMail(mailOptions, (err, info) => {
 	if(err) {
 	    console.error(chalk.red('Error while send email:'), err);
 	} else {
-	    console.log(chalk.yellow('Good info:'), info);
+	    console.log(chalk.yellow('Mail sended with info:'), info);
 	}
     });
 }
